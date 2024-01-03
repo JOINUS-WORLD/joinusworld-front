0f199f5969153fe9e21c135d9459cf0f684de19e
Doyu-Lee <doyu.lee.0406@gmail.com>
2024-01-04 00:50:42 +0900

    [JUW-85] feat(pac/ui): create heading component

---
+++ packages/ui-library/components/layout/src/typography/Heading.tsx
@@ -0,0 +1,46 @@
+import { vars } from "@join-world/themes";
+import { clsx } from "clsx";
+import * as React from "react";
+import { forwardRef, Ref } from "react";
+
+import { BaseStyle, StyleSprinkles } from "../core/style.css";
+import { extractSprinkleProps } from "../utils/properties";
+import { textStyle } from "./style.css";
+import { HeadingProps } from "./types";
+
+const Heading = (props: HeadingProps, ref: Ref<HTMLElement>) => {
+  const {
+    as = "h1",
+    fontSize,
+    background,
+    color = "gray300",
+    children,
+  } = props;
+
+  return React.createElement(
+    as,
+    {
+      ...props,
+      ref,
+      className: clsx([
+        BaseStyle,
+        StyleSprinkles(
+          extractSprinkleProps(props, Array.from(StyleSprinkles.properties)),
+        ),
+        textStyle({
+          fontSize,
+        }),
+        props.className,
+      ]),
+      style: {
+        color: color && vars.colors.mode.ref.palette[color],
+        background: background && vars.colors.mode.ref.palette[background],
+        ...props.style,
+      },
+    },
+    children,
+  );
+};
+
+const _Heading = forwardRef(Heading);
+export { _Heading as Heading };
