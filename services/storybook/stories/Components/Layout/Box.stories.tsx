50cc8a54e092928aa084814271b41fdf6c9b44bf
Doyu-Lee <doyu.lee.0406@gmail.com>
2024-01-04 00:47:37 +0900

    [JUW-85] chore(ser/sb): add argTypes to Box stories

--- services/storybook/stories/Components/Layout/Box.stories.tsx
+++ services/storybook/stories/Components/Layout/Box.stories.tsx
@@ -1,5 +1,6 @@
 import "@join-world/ui-components-layout/style.css";
 
+import { vars } from "@join-world/themes";
 import { Box as _Box } from "@join-world/ui-components-layout";
 
 export default {
@@ -9,6 +10,20 @@ export default {
     layout: "centered",
   },
   tags: ["autodocs"],
+  argTypes: {
+    padding: {
+      options: Object.keys(vars.sys.spacing),
+      control: "select",
+    },
+    background: {
+      options: Object.keys(vars.colors.mode.ref.palette),
+      control: "select",
+    },
+    boxShadow: {
+      options: Object.keys(vars.sys.shadow),
+      control: "select",
+    },
+  },
 };
 
 // 예시
@@ -16,10 +31,7 @@ export const BoxStory = {
   args: {
     as: "button",
     padding: "size9",
-    color: "blue",
-    background: "blue900",
-    width: "100px",
-    height: "50px",
+    background: "blue50",
     boxShadow: "level3",
   },
 };
