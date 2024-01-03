dd8810943c82bd17b491719141c2c730f6fa4ec4
Doyu-Lee <doyu.lee.0406@gmail.com>
2024-01-04 00:50:08 +0900

    [JUW-85] feat(pac/ui): create text component

---
+++ packages/ui-library/components/layout/src/typography/Text.tsx
@@ -0,0 +1,39 @@
+import { vars } from "@join-world/themes";
+import { clsx } from "clsx";
+import * as React from "react";
+
+import { BaseStyle, StyleSprinkles } from "../core/style.css";
+import { extractSprinkleProps } from "../utils/properties";
+import { textStyle } from "./style.css";
+import { TextProps } from "./types";
+
+const Text = (props: TextProps, ref: React.Ref<HTMLElement>) => {
+  const { as = "p", color = "gray50", background, children, fontSize } = props;
+
+  return React.createElement(
+    as,
+    {
+      ...props,
+      ref,
+      className: clsx([
+        BaseStyle,
+        StyleSprinkles(
+          extractSprinkleProps(props, Array.from(StyleSprinkles.properties)),
+        ),
+        textStyle({
+          fontSize,
+        }),
+        props.className,
+      ]),
+      style: {
+        color: color && vars.colors.mode.ref.palette[color],
+        background: background && vars.colors.mode.ref.palette[background],
+        ...props.style,
+      },
+    },
+    children,
+  );
+};
+
+const _Text = React.forwardRef(Text);
+export { _Text as Text };
