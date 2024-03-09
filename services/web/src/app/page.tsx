0f1db9925fa396f7ee2bac413232c6196a594a65
wjbetech <wjbetech@gmail.com>
2024-03-09 12:22:07 +0900

    Added file structure for web/*

--- services/web/src/app/page.tsx
+++ services/web/src/app/page.tsx
@@ -1,13 +1,16 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-import Loading from "./Loading";
+// import Loading from "./Loading";
+
+import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Home</h1>
-      <Link href="/test">test</Link>
-      <Loading />
+      {/* <Link href="/test">test</Link> */}
+      {/* <Loading /> */}
     </div>
   );
 }
